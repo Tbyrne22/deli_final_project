d21a042986e00840deb94de5535c484756623f05
Roderic Henry <roderichenry@gmail.com>
2023-12-16 23:22:52 -0500

    Cookie consent mobile styles

--- components/CookieBanner.js
+++ components/CookieBanner.js
@@ -15,7 +15,7 @@ function Transition(props) {
 
 const cookieContainer = {
   backgroundColor:'#FFFFFF',
-  height: '358px',
+  height: {xs:'auto', md:'358px'},
   position: 'fixed',
   bottom:0,
   zIndex:10,
@@ -28,6 +28,7 @@ const buttonCss = {
   color:'#FFFFFF',
   fontWeight:500,
   ml:'40px',
+  mt:{xs:'24px', md:'0'},
   lineHeight:'16px',
 };
 
@@ -86,7 +87,8 @@ export default function CookieBanner() {
 
   return (
     <Box sx={{...cookieContainer, display:`${open ? "block":"none"}`}}>
-      <Box sx={{ width:'1256px', mx:'auto', padding:'58px 0 118px 0', position:'relative'}}>
+      <Box sx={{ width:{xs:'auto', md:'892px', lg:'1256px'}, mx:{xs:3, md:'auto'}, 
+        padding:{xs:'24px 0', md:'58px 0 118px 0'}, position:'relative'}}>
         <Typography align="center" variant="h4" component="h4" sx={{fontFamily:'Work Sans', fontSize:30, fontWeight:400}}>
           Cookies Policy Notification
         </Typography>
@@ -95,13 +97,13 @@ export default function CookieBanner() {
           border:'solid 1px #CCCCCC', 
           backgroundColor:'#FFF5F8', 
           pl:'10px', pr:'20px', py:'18px', 
-          mt:'55px', 
-          display:'flex',
+          mt:{xs:'24px', md:'55px'}, 
+          display: {xs:'block', md:'flex'},
           alignItems:'center',
         }}
         >
-          <InfoIcon />
-          <Box sx={{width:'815px', ml:'10px'}}>
+          <InfoIcon sx={{display:{xs: 'none', md: 'block'} }} />
+          <Box sx={{width:{xs:'auto', md:'460px', lg:'815px'}, ml:'10px'}}>
             <Typography sx={{fontFamily:'Roboto'}}>
               We use cookies on our website to enhance your browsing experience. By clicking 'Accept,' you agree to 
               the use of cookies as described in our Cookie Policy. You can manage your preferences and learn more 
@@ -109,7 +111,7 @@ export default function CookieBanner() {
             </Typography>
           </Box>
 
-          <Button sx={{...buttonCss, ...rejectCss}} onClick={rejectCookie}>Reject Cookies</Button>
+          <Button sx={{...buttonCss, ...rejectCss, ml:{xs:'10px', md:'40px'}}} onClick={rejectCookie}>Reject Cookies</Button>
           <Button sx={{...buttonCss, ...acceptCss}} onClick={acceptCookie} >Accept Cookies</Button>
         </Box>
       </Box>
