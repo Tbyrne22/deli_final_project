e7845d72587a064830fb2d9c726789c83e9b4106
Roderic Henry <roderichenry@gmail.com>
2023-12-18 22:54:10 -0500

    Section titles show when you go to that section from link. Closes #166

--- app/modules/views/Specials.js
+++ app/modules/views/Specials.js
@@ -16,51 +16,52 @@ const specialSection = {
 
 export default function Specials() {
   return (
-    <Box component="section" sx={{...specialSection}}>
+    <>
       <a name="specials"></a>
-      <Container>        
-        <Typography color="inherit" align="center" variant="h2" component="h2" marked="center">
-          Specials
-        </Typography>      
-        <Typography color="inherit" align="center" variant="body1" >
-          Rotates at the end of each month.<br />Order soon to not miss out!
-        </Typography>
+      <Box component="section" sx={{...specialSection}}>
         
-        <Container sx={{mt:'48px'}}>
-          <Grid container>
-            <Grid item xs={12} md={4}>
-              <MediaCard 
-                title="Monthly Special" 
-                image="/images/card-image-1.png"
-                text="Vietnamese-style Lemongrass Chicken Bahn-mi in a freshly baked french baguette."
-                price="12.99"
-                ends="12/31"
-              />
+        <Container>        
+          <Typography color="inherit" align="center" variant="h2" component="h2" marked="center">
+            Specials
+          </Typography>      
+          <Typography color="inherit" align="center" variant="body1" >
+            Rotates at the end of each month.<br />Order soon to not miss out!
+          </Typography>
+          
+          <Container sx={{mt:'48px'}}>
+            <Grid container>
+              <Grid item xs={12} md={4}>
+                <MediaCard 
+                  title="Monthly Special" 
+                  image="/images/card-image-1.png"
+                  text="Vietnamese-style Lemongrass Chicken Bahn-mi in a freshly baked french baguette."
+                  price="12.99"
+                  ends="12/31"
+                />
+              </Grid>
+              <Grid item xs={12} md={4}>
+                <MediaCard 
+                  title="Discount Special 1" 
+                  image="/images/card-image-2.png"
+                  text="Smoked Turkey Hero with candied walnuts - substitute with portobello mushrooms."
+                  price="11.99"
+                  discount="4"
+                />
+              </Grid>
+              <Grid item xs={12} md={4}>
+                <MediaCard 
+                  title="Discount Special 2" 
+                  image="/images/card-image-3.png"
+                  text="Roasted Pork Hero with dill-dijon mustard sauce in a seeded ciabatta roll."
+                  price="10.99"
+                  discount="3"
+                />
+              </Grid>
             </Grid>
-            <Grid item xs={12} md={4}>
-              <MediaCard 
-                title="Discount Special 1" 
-                image="/images/card-image-2.png"
-                text="Smoked Turkey Hero with candied walnuts - substitute with portobello mushrooms."
-                price="11.99"
-                discount="4"
-              />
-            </Grid>
-            <Grid item xs={12} md={4}>
-              <MediaCard 
-                title="Discount Special 2" 
-                image="/images/card-image-3.png"
-                text="Roasted Pork Hero with dill-dijon mustard sauce in a seeded ciabatta roll."
-                price="10.99"
-                discount="3"
-              />
-            </Grid>
-          </Grid>
+          </Container>
+
         </Container>
-
-      </Container>
-    </Box>
-
-   
+      </Box>
+    </>   
   );
 }
