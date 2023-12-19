12c6796d0520eff2c9e4f92bbbb65d2bd0aa7242
Roderic Henry <roderichenry@gmail.com>
2023-12-18 22:39:36 -0500

    Original price alignment in specials section. Closes #165

--- app/modules/views/MediaCard.js
+++ app/modules/views/MediaCard.js
@@ -3,7 +3,6 @@ import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
 export default function MediaCard(props) {
@@ -15,6 +14,7 @@ export default function MediaCard(props) {
     color: '#000000',
     textDecoration: 'line-through',
     fontSize:16,
+    ml:'auto !important'
   };
   const markup = (props.discount ? (parseFloat(props.price) + parseFloat(props.discount)) : null);
   const priceText = '$' + props.price + (props.discount ? ' ($' + props.discount + ' OFF)' : '');
@@ -43,9 +43,11 @@ export default function MediaCard(props) {
         <Typography sx={{...discountStyle}}>
           {discountText}
         </Typography>
+        { endText != '' ? (
         <Typography sx={{color:"#FF0000", fontSize:16, ml:'auto !important'}}>
           {endText}
-        </Typography>        
+        </Typography>
+        ) : ''}
       </CardActions>
     </Card>
   );
